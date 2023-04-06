0b68bbbfaecd925f0c4bf96b6e187a3185ad13e6
yangjie <yangjie@ihealthlabs.com.cn>
2023-04-06 10:56:48 +0800

    域名改回去

--- src/main/java/com/sandglass/sandglasslibrary/net/SLFHttpRequestConstants.java
+++ src/main/java/com/sandglass/sandglasslibrary/net/SLFHttpRequestConstants.java
@@ -9,10 +9,10 @@ public class SLFHttpRequestConstants {
     public static final String APP_ID = "test";
     //public static final String BASE_URL = "http://app.gateway.chenyanjin.top";
     //public static final String BASE_URL = "http://172.16.100.230:10003";
-    //public static final String BASE_URL = "https://rvi-app-gateway.kivolabs.com";
+    public static final String BASE_URL = "https://rvi-app-gateway.kivolabs.com";
     //新版测服入口
     //public static final String BASE_URL = "https://rvi-test-app-gateway.hualaikeji.com";
-    public static  String BASE_URL = SLFCheckService.BASE_URL;
+    //public static  String BASE_URL = SLFCheckService.BASE_URL;
 
     public static final String APP_KEY = "test";
 
